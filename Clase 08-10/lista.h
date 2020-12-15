370da2dca263c81be78ad0baae4ff990386fccdd
Pedro Munoz <49573624+pedromxnoz@users.noreply.github.com>
2020-12-15 17:41:28 -0300

    Agregado TDA de Lista de las Clase 08 a 10

---
+++ Clase 08-10/lista.h
@@ -0,0 +1,17 @@
+//Incluir el archivo de Estructuras
+#include "listaStruct.h"
+
+//Prototipos
+bool vacia(Lista lista);
+int primero(Lista lista);
+int fin(Lista lista);
+
+//Clase 10/12
+int anterior(int p, Lista lista);
+int siguiente(int p, Lista lista);
+void inserta(tipoDato x, int p, Lista &lista);
+tipoDato recupera (int p, Lista lista);
+void imprimeLista(Lista lista);
+int localiza(tipoDato x, Lista lista);
+void suprime(int p, Lista &lista);
+void anula(Lista &lista);
